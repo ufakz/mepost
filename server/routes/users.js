be8938b2e520b251bce1fdf13276681306eae7cb
Farouq Umar <ufakz99@gmail.com>
2022-03-23 16:36:42 +0100

    feat: create users routes

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+const usersController = require('./../controllers/user.ctrl')
+
+const usersRoute = (router) => {
+    //Get a user
+    router.route('/users/:id').get(usersController.getUser)
+
+    //Get a user profile
+    router.route('/users/profile/:id').get(usersController.getUserProfile)
+
+    //Adds a user
+    router.route('/users').post(usersController.addUser)
+
+    //Follow a user
+    router.route('/users/follow').post(usersController.followUser)
+}
