4b51cb39389a59fba188589d909dd2d040b60e9f
Farouq Umar <ufakz99@gmail.com>
2022-03-24 11:17:47 +0100

    feat: implment user reducer

---
+++ src/redux/reducers/authUser.js
@@ -0,0 +1,35 @@
+const initialState = {
+    user: {},
+    isAuth: false,
+    profile: {}
+}
+
+const userAuthReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                isAuth: Object.keys(action.user).length > 0 ? true : false,
+                user: action.user
+            }
+
+        case 'FOLLOW_USER':
+            let user = Object.assign({}, state.user)
+            user.following.push(action.user_id)
+            return {
+                ...state,
+                user: user
+            }
+
+        case 'SET_PROFILE':
+            return {
+                ...state,
+                profile: action.profile
+            }
+
+        default:
+            return state
+    }
+}
+
+export default userAuthReducer
